4101f1591373fd154ed9d1f3d88c63c4c72a1e42
Wayamanna <wayamanna2@gmail.com>
2025-11-23 19:17:26 +0200

    added countWords function

--- test.js
+++ test.js
@@ -17,5 +17,31 @@ function longNames(names) {
 // اختبارات
 console.log("Sum Number:", sumArry([1, 2, 3, 4, 5]));
 console.log(" Long names:", longNames(["Ali", "Fatima", "Omar", "Sarah"]));
+
+function countWords(str) {
+    // إزالة المسافات الزائدة من أول وآخر الجملة
+    str = str.trim();
+
+    // تقسيم الجملة لكلمات باستخدام الفراغ كفاصل
+    let words = str.split(/\s+/); // \s+ يعني أي مسافة أو أكثر
+
+    // إنشاء object لتخزين عدد كل كلمة
+    let wordCount = {};
+
+    for (let word of words) {
+        // إذا الكلمة موجودة بالفعل زد العدد، إذا لا خليها 1
+        if (wordCount[word]) {
+            wordCount[word]++;
+        } else {
+            wordCount[word] = 1;
+        }
+    }
+
+    return wordCount;
+}
+
+console.log(countWords("apple banana apple orange apple banana"));
+
+
         
     
